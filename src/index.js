5bc92237e2d299f877ccb1c7d6273440bc18da6e
vermasumeet1982 <vermasumeet1982@gmail.com>
2022-02-16 17:52:33 +0000

    naive approach

--- src/index.js
+++ src/index.js
@@ -1,25 +1,63 @@
 // Import the React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from '@faker-js/faker';
 
 // Create a React component
 const App = () => {
-    const buttonText = { text: 'Click Me Object!!' };
-    const buttonStyle = {
-        backgroundColor :'blue', 
-        color: 'white',
-        border: '3px solid green'
-    };
     return (
-        <div>
-            <label className="label" htmlFor="name">
-                 Enter name 
-            </label> 
-            <br/>
-            <input id="name" type="text" /> 
-            <br/>
-            <br/>
-            <button style={buttonStyle}> {buttonText.text} </button>
+        <div className='ui container comments'>
+            <div className='comment'>
+                <a href='/' className='avatar'>
+                    <img alt="avatar" src={faker.image.avatar()}/>
+                </a>
+                <div className="content">
+                    <a href='/' className='author'>
+                        Sam
+                    </a>
+                    <div className='metadata'>
+                        <span className='date'>Today at 8:00 pm</span>
+                    </div>
+                    <div className='text'>
+                        Nice Blog Post!
+                    </div>
+                </div>
+            </div>
+
+            <div className='comment'>
+                <a href='/' className='avatar'>
+                    <img alt="avatar" src={faker.image.avatar()}/>
+                </a>
+                <div className="content">
+                    <a href='/' className='author'>
+                        George
+                    </a>
+                    <div className='metadata'>
+                        <span className='date'>Yesterday at 12:00 pm</span>
+                    </div>
+                    <div className='text'>
+                        Very insighful!
+                        How do you do this??
+                    </div>
+                </div>
+            </div>
+
+            <div className='comment'>
+                <a href='/' className='avatar'>
+                    <img alt="avatar" src={faker.image.avatar()}/>
+                </a>
+                <div className="content">
+                    <a href='/' className='author'>
+                        Sophie
+                    </a>
+                    <div className='metadata'>
+                        <span className='date'>29/01/2022 at 11:30 pm</span>
+                    </div>
+                    <div className='text'>
+                        I don't like it!
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
