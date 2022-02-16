5580aaf7a61d22a0d83e6578616dfdcfc44d201c
vermasumeet1982 <vermasumeet1982@gmail.com>
2022-02-16 18:59:02 +0000

    added comment component

---
+++ src/components/Comment.js
@@ -0,0 +1,24 @@
+import faker from '@faker-js/faker';
+
+const Comment = ({name, time, body, image}) => {
+    return(
+        <div className='comment'>
+                <a href='/' className='avatar'>
+                    <img alt="avatar" src={image ? image : faker.image.food()}/>
+                </a>
+                <div className="content">
+                    <a href='/' className='author'>
+                        {name}
+                    </a>
+                    <div className='metadata'>
+                        <span className='date'>{time}</span>
+                    </div>
+                    <div className='text'>
+                       {body}
+                    </div>
+                </div>
+            </div>
+    );
+};
+
+export default Comment;
--- src/index.js
+++ src/index.js
@@ -1,63 +1,30 @@
 // Import the React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Comment from './components/Comment';
 import faker from '@faker-js/faker';
 
 // Create a React component
 const App = () => {
     return (
         <div className='ui container comments'>
-            <div className='comment'>
-                <a href='/' className='avatar'>
-                    <img alt="avatar" src={faker.image.avatar()}/>
-                </a>
-                <div className="content">
-                    <a href='/' className='author'>
-                        Sam
-                    </a>
-                    <div className='metadata'>
-                        <span className='date'>Today at 8:00 pm</span>
-                    </div>
-                    <div className='text'>
-                        Nice Blog Post!
-                    </div>
-                </div>
-            </div>
-
-            <div className='comment'>
-                <a href='/' className='avatar'>
-                    <img alt="avatar" src={faker.image.avatar()}/>
-                </a>
-                <div className="content">
-                    <a href='/' className='author'>
-                        George
-                    </a>
-                    <div className='metadata'>
-                        <span className='date'>Yesterday at 12:00 pm</span>
-                    </div>
-                    <div className='text'>
-                        Very insighful!
-                        How do you do this??
-                    </div>
-                </div>
-            </div>
-
-            <div className='comment'>
-                <a href='/' className='avatar'>
-                    <img alt="avatar" src={faker.image.avatar()}/>
-                </a>
-                <div className="content">
-                    <a href='/' className='author'>
-                        Sophie
-                    </a>
-                    <div className='metadata'>
-                        <span className='date'>29/01/2022 at 11:30 pm</span>
-                    </div>
-                    <div className='text'>
-                        I don't like it!
-                    </div>
-                </div>
-            </div>
+            <Comment 
+                name='Samuel Webb' 
+                time='Yesterday at 6:00 am' 
+                body='Hi there!!' 
+                image={faker.image.transport()} 
+            />
+            <Comment 
+                name='Peter Johnson' 
+                time='Yesterday at 5:00 am' 
+                body='How are ya!!' 
+            />
+            <Comment 
+                name='Sophie Jackson' 
+                time='22/01/2022 at 6:00 am' 
+                body='I really really don&apos;t like this at all!!' 
+                image={faker.image.avatar()}
+            />
         </div>
     );
 }
