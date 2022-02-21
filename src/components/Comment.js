34fda36bdbd0fb9b37f4331882b956176f811ae6
vermasumeet1982 <vermasumeet1982@gmail.com>
2022-02-21 13:30:44 +0000

    minor changes

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -4,21 +4,21 @@ import faker from '@faker-js/faker';
 const Comment = ({name, time, body, image}) => {
     return(
         <div className='comment'>
-                <a href='/' className='avatar'>
-                    <img alt="avatar" src={image ? image : faker.image.food()}/>
+            <a href='/' className='avatar'>
+                <img alt="avatar" src={image ? image : faker.image.food()}/>
+            </a>
+            <div className="content">
+                <a href='/' className='author'>
+                    {name}
                 </a>
-                <div className="content">
-                    <a href='/' className='author'>
-                        {name}
-                    </a>
-                    <div className='metadata'>
-                        <span className='date'>{time}</span>
-                    </div>
-                    <div className='text'>
-                       {body}
-                    </div>
+                <div className='metadata'>
+                    <span className='date'>{time}</span>
+                </div>
+                <div className='text'>
+                    {body}
                 </div>
             </div>
+        </div>
     );
 };
 
